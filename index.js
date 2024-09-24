b1ff8f7db9167687b91f8f3cc1752a5c8c39007b
junwon_lee <wjdrbs307@naver.com>
2024-09-25 00:26:23 +0900

    기초 문법 공부

---
+++ index.js
@@ -0,0 +1,12 @@
+var Name2 = 'lee'; //변수설정(string만들어올수있음)
+var Name = 'lee'; //변수설정(string만들어올수있음)
+function example1(x) {
+    return x * 2;
+}
+var john = { name: 'kim' };
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-let 이름 = 'lee';
+// let 이름 : string[]  = ['kim', 'park];
+// let 이름 : {name? : string} = { name : 'kim' }
+// let 이름 :string | number = 'lee'; 
+type MyType = string | number;// 타입은 변수에 담아 쓸 수 있음 
+let Name2 : MyType = 'lee'; //변수설정(string만들어올수있음)
+let Name : string = 'lee'; //변수설정(string만들어올수있음)
+
+function example1(x : number) :number {
+    return x * 2
+}
+
+//type Member = [number, boolean];
+//let john:Member = [123, true];
+
+type Member = {
+    [key  : string] : string, // 글자로 된 모든 object속성의 타입은 : string 
+}
+let john : Member = { name : 'kim'}
+
+class User {
+    name : string;
+    constructor(name){
+        this.name = name;
+    }
+}
