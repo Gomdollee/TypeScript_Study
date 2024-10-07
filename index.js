50b4a104d020254d86baf20cf36493b93b333ce5
junwon_lee <wjdrbs307@naver.com>
2024-10-08 01:09:52 +0900

    타입스크립트 변수

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var Name2 = 'lee'; //변수설정(string만들어올수있음)
 var Name = 'lee'; //변수설정(string만들어올수있음)
+var age = 50;
+var areumarry = false;
 function example1(x) {
     return x * 2;
 }
+var john2 = [123, true];
+//오브젝트 
+var Member3 = { member1: 'kim', member2: 'lee' };
 var john = { name: 'kim' };
 var User = /** @class */ (function () {
     function User(name) {
@@ -10,3 +15,8 @@ var User = /** @class */ (function () {
     }
     return User;
 }());
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -4,13 +4,21 @@
 type MyType = string | number;// 타입은 변수에 담아 쓸 수 있음 
 let Name2 : MyType = 'lee'; //변수설정(string만들어올수있음)
 let Name : string = 'lee'; //변수설정(string만들어올수있음)
-
+let age :number = 50;
+let areumarry :boolean = false;
+ 
 function example1(x : number) :number {
     return x * 2
 }
 
-//type Member = [number, boolean];
-//let john:Member = [123, true];
+// 사실 타입지정안해도 자동으로 해줌 (타입지정 문법 생력가능)
+
+//배열
+type Member2 = [number, boolean];
+let john2:Member2 = [123, true];
+
+//오브젝트 
+let Member3 :{ member1 : string , member2 : string} = { member1 : 'kim' , member2 : 'lee'}
 
 type Member = {
     [key  : string] : string, // 글자로 된 모든 object속성의 타입은 : string 
@@ -18,8 +26,14 @@ type Member = {
 let john : Member = { name : 'kim'}
 
 class User {
-    name : string;
+    name : string; 
     constructor(name){
         this.name = name;
     }
+}
+
+let project : {member :string[], days :number, started :boolean } = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
 }
